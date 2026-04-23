650ab0e0e24093afab2fa9cd92c51c609af3797e
shivangii123 <shivangigupta2801@gmail.com>
2026-04-24 00:42:44 +0530

    update file

--- NodeJsNotes/CRUD_Operation/writeToFile.js
+++ NodeJsNotes/CRUD_Operation/writeToFile.js
@@ -18,37 +18,39 @@
 
 // console.log("Hi I am another piece of Code"); //prints before "data written.."
 
-//====================================================================
-// //--------------------- Create file using :- promise----------------------
-// let fs = require('fs/promises') ;
 
-// const path = require('path') ;
-// let filePath = path.join(__dirname, 'song.txt' );
+// ====================================================================
+//--------------------- Create file using :- promise----------------------
+let fs = require('fs/promises') ;
 
-// fs.writeFile(filePath, " HEY !! HEY !!" ,{flag :'a'})
-// .then( ()=>
-//     console.log("data succesfully written "))
-// .catch(err =>
-//      console.log(err)
-// )
-// console.log("hello i am simple print statment");//yeh pehle chalegi->bcoz writefile asynchronous h
+const path = require('path') ;
+let filePath = path.join(__dirname, 'song.txt' );
+
+fs.writeFile(filePath, " HEY !! HEY !!" ,{flag :'a'})
+.then( ()=>
+    console.log("data succesfully written "))
+.catch(err =>
+     console.log(err)
+)
+console.log("hello i am simple print statment");//yeh pehle chalegi->bcoz writefile asynchronous h
 
    
-// ====================================================================
-//--------------------- Create file using :- Async/Await----------------------
-const fs = require('fs/promises') ;
-const path = require('path');
 
-let content = ["asnmdlks", "sdsds", "asdasd"] ;
+// // ====================================================================
+// //--------------------- Create file using :- Async/Await----------------------
+// const fs = require('fs/promises') ;
+// const path = require('path');
 
-async function writeData(fileName) {
-    let filePath = path.join(__dirname, '..','database', fileName) ;
-    await fs.writeFile(filePath, JSON.stringify(content));
-    console.log("write sucess");
-}
+// let content = ["asnmdlks", "sdsds", "asdasd"] ;
 
-writeData('course.json');
-console.log("end of program")
+// async function writeData(fileName) {
+//     let filePath = path.join(__dirname, '..','database', fileName) ;
+//     await fs.writeFile(filePath, JSON.stringify(content));
+//     console.log("write sucess");
+// }
+
+// writeData('course.json');
+// console.log("end of program")
 
 
 // // //====================================================================
