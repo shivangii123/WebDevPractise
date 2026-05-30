120d5f6fe05dddac7b5c4322206438253085184a
shivangii123 <shivangigupta2801@gmail.com>
2026-05-30 23:48:53 +0530

    mongoose file'

--- Mongoose/app.js
+++ Mongoose/app.js
@@ -1,57 +1,17 @@
-const path = require('path');
 const express = require('express');
-const mongoose = require('mongoose');
-const customers = require('./schema/customer');
-const order = require('./schema/order');
 const app = express();
+const path = require('path');
 const PORT = 4444;
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public'))) ;
 
+app.get('/', (req, res) => {
+  console.log('Home page');
+  res.send('welcome all of you ..!!!\n how are you');
+});
 
-app.post('/orders', async (req, res) => {
-    const {productName, price, customerId} = req.body;
-
-    let newOrder = await order.insertOne({
-        productName,
-        customerId,
-        price
-    })
-    res.status(200).json({
-        
-        message: 'Order Created Successfully',
-        newOrder
-    })
-})
-
-app.post('/customer', async (req, res) => {
-    const {name, email} = req.body;
-    console.log(name, email);
-    let newCustomer = await customers.insertOne({
-        name,
-        email
-    })
-
-    res.status(200).json({
-        message: 'Customer Created Successfully',
-        newCustomer
-    })
-})
-
-app.get('/orders', async (req, res) => {
-    const {customerId} = req.query;
-    let orderData = await order.find({
-        customerId
-    })
-    .populate({
-        path: 'customerId',
-        select : 'name'
-    })
-
-    res.send(orderData);
-})
-
-app.listen(PORT, ()=>{
-    console.log(`listening to ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log('Listening to http://localhost:' + PORT);
+});
