94f64dce589315ff492e7fedc979dd241435dc16
shivangii123 <shivangigupta2801@gmail.com>
2026-05-31 23:46:45 +0530

    mongoose connection

--- Mongoose/app.js
+++ Mongoose/app.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 4444;
+const mongoose = require('mongoose') ;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public'))) ;
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/Test')
+  .then((data)=>{
+    console.log("DB conected");
+  })
+  .catch((err)=>{
+    console.log(err.message);
+  })
 
 app.get('/', (req, res) => {
   console.log('Home page');
