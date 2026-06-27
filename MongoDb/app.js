48f21ef557e1a2d68b3b48a22b4a4cbc168f3a21
shivangii123 <shivangigupta2801@gmail.com>
2026-06-27 23:48:57 +0530

    mongodb

--- MongoDb/app.js
+++ MongoDb/app.js
@@ -1,68 +1,64 @@
-const express = require('express'); //Load Express module into this file
-const app = express(); //Create Express application object
-const PORT = 3000;
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 4444;
 
-app.use(express.json()); // axios send json  data
-app.use(express.urlencoded({extended :true}));
-
-//MongoDb and NodeJs connection code
-const { MongoClient } = require('mongodb'); // import mongoClient
-
-// Connection URL
-const url = 'mongodb://localhost:27017'; //By default port of MongoDb
-const client = new MongoClient(url); //Create MongoDB client object
-
-// Database Name
-const dbName = 'testDb';//if DB exists use it , else create new one
-
-async function main() {
-    
-    await client.connect() ; //Connect Node.js app to MongoDB server.
-    console.log("NODEJs successfully connected to Mongodb");
-
-    const db = client.db(dbName) ; //db()->Select database 
-    const users = db.collection('users'); //collection()-> Select Collection
-
-    // users.find({}).toArray()//DB se data nikalme mein time lagega so it's a Promise
-    //     .then( (data)=>{
-    //         console.log( data);
-    //     })
-    //     .catch(err => 
-    //         console.log(err)
-    //     )
-
-    // const currResult = await users.find({}).toArray() ;
-    // console.log(`Current documents :`,currResult);
-            
-    //find
-    users.find({name :'shivangi'}).toArray()
-        .then((data)=> console.log(data))
-        .catch(err =>console.log(err))
-
-    const fetchedInfo = await users.find({name:'shivangi'}).toArray() ;
-    console.log('fetched info is =>', fetchedInfo);
-
-    // Insert one document (row)
-    users.insertOne({name: 'shivangi', age: 21, location:'pune'  })  
-    console.log('Inserted a document');
-    
-    // update
-    const updateInfo = await users.updateOne({name:'siya'} , {$set :{ name :'tina'}})
-    console.log('Updated info is =>', updateInfo);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
-    // const deleteResult = await users.deleteMany({name:"shivangi" });
-    // console.log(`Deleted documents :`, deleteResult);
+const {MongoClient} =require('mongodb') ; 
 
-    return 'done' ;   
+const url = 'mongodb://localhost:27017'; //Mongodb url
+const client = new MongoClient(url) ;
+
+const dbName = 'testDb' ;
+
+async function main(){
+    await client.connect(); // connect to mongodb server
+    console.log("succesfully connected to mongodb");
+    const db = client.db('testDb') ;
+    const users = db.collection('users');
+
+
+    // //insert
+    // const insertResult = await users.insertOne({name:'riya',age:22,location:'pune'})
+    // console.log(`Inserted documents => ${insertResult}`);
+
+    // //upate
+    // const updateResult = await users.updateOne({name:"maya"},{$set: {name:"siya"}})
+    // console.log('Updated document=>', updateResult);
+
+    // // delete
+    // await users.deleteOne({name:'riya'});
+
+    //read
+    const findResult = await users.find({}).toArray() ;
+    console.log('found documents =>' ,findResult);
+
+    await users.insertOne({task: 'dance' , status:'false', id = uuid()});
+
+    await users.find({}).toArray() ;
+
+    await users.updateMany({ id :'given_id'}, {$set:{status:'false'}} ) ;
+
+    await users.deleteOne({id: 'given_id'});
+
+    return 'done' ;
 }
 
+app.get('/', (req, res) => {
+  console.log('Home page');
+  res.send('welcome all of you ..!!!\n how are you');
+});
+
+
 main()
-    .then(()=>{
-        app.listen(PORT , ()=>{ // start server
-            console.log("Listening to http://localhost:"+PORT) ;
-        })
-    })
-    .catch(err=>{
-        console.log(err.message);
-    })
+  .then(()=>{
+    app.listen(PORT, ()=>{
+      console.log('Listening to http://localhost:'+PORT);
+    }) ;
+  })
+  .catch(err => console.log(err)) 
+
+
