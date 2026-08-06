06a83f3324c38c9468115869aa757a3e2134dae9
shivangii123 <shivangigupta2801@gmail.com>
2026-08-06 23:55:19 +0530

    change

--- Cookies-JWT/app2.js
+++ Cookies-JWT/app2.js
@@ -39,7 +39,7 @@ app.get('/check-token', (req, res)=>{
 
   } catch (error) {
     res.send({
-      msg : "invalid Token, give correct one!",
+      msg : "invalid Token, enter correct!!",
       err: error.message
      })
   }
